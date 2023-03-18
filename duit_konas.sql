de448cc9dbb2f9d50bc609f682c84fe22b33cece
azern32 <chiro.emiya@gmail.com>
2023-03-18 13:39:55 +0800

    database pake uuid

--- duit_konas.sql
+++ duit_konas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2023 at 10:53 PM
+-- Generation Time: Mar 18, 2023 at 06:39 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `akun_hutang` (
+  `uuid` char(38) NOT NULL,
   `kode_akun` char(38) NOT NULL,
   `nama_akun` char(255) NOT NULL,
   `kredit` bigint(20) NOT NULL,
@@ -41,12 +42,20 @@ CREATE TABLE `akun_hutang` (
 --
 
 CREATE TABLE `daftar_akun` (
+  `uuid` char(38) NOT NULL,
   `kode_akun` char(38) NOT NULL,
   `nama_akun` char(255) NOT NULL,
   `kredit` bigint(20) NOT NULL,
   `debit` bigint(20) NOT NULL,
   `saldo` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `daftar_akun`
+--
+
+INSERT INTO `daftar_akun` (`uuid`, `kode_akun`, `nama_akun`, `kredit`, `debit`, `saldo`) VALUES
+('1d473688-a0fc-4c9e-a62e-f5f741b04f58', '111', 'Kas Tunai', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -67,6 +76,18 @@ CREATE TABLE `daftar_logs` (
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `akun_hutang`
+--
+ALTER TABLE `akun_hutang`
+  ADD PRIMARY KEY (`uuid`);
+
+--
+-- Indexes for table `daftar_akun`
+--
+ALTER TABLE `daftar_akun`
+  ADD PRIMARY KEY (`uuid`);
 
 --
 -- Indexes for table `daftar_logs`
